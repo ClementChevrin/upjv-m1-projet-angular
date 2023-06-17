aabd8f05e08994f386aa39f626ac72af5df27d6e
RyanNgae <ryan02.nm@gmail.com>
2023-06-17 18:45:23 +0200

    Create getMyProjects.js

---
+++ backend/functions/getMyProjects.js
@@ -0,0 +1,39 @@
+import User from '../models/user.js'
+
+export var getAllMyProject = async (req, res) => {
+
+    try {
+    
+        let myProject = await User.aggregate([
+            {
+                $match: { email : req.body.email}
+            },
+            {
+                $unwind: "$projets"
+            },
+            {
+                $lookup:{
+                    from: "projets",
+                    localField: "projets",
+                    foreignField : "codeP",
+                    as : "myProjects"
+                }
+            },
+            {
+                $project :{
+                    "_id" : 0,
+                    "projets._id" : 0
+                }
+            }
+            
+        ]);
+
+        res.json(myProject);
+    }
+    catch(err){
+
+        console.log(err);
+
+    }
+}
+
